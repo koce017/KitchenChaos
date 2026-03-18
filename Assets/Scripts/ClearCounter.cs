0a43b72fe1f773489e432053ad5b57ad11112a38
koce017 <koce017@mail.com>
2026-03-18 19:26:13 +0100

    Kitchen Object, Scriptable Objects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,10 +1,14 @@
+using System;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
     public void Interact()
     {
-        Debug.Log("Interacted with ClearCounter");
+        Transform kitchenObjecte = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        kitchenObjecte.localPosition = Vector3.zero; // set the local position to zero to place it at the counter top point
     }
 }
